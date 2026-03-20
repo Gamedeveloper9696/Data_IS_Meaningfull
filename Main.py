e800c922e6cb6e41f789427a65dc175cfb7b33f8
Poornima <poornisubramani@gmail.com>
2026-03-20 14:45:55 +0000

    messege

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 def great_user():
     name = input("what is your name?")
-    print(f"Hello, {name}! welcome to the program3.")
+    print(f"Hello, {name}! welcome to the program4.")
 
 #call the function
 great_user()
